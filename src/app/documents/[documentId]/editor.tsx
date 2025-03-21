7c6c4a3ede213396bf152b663daf695ffec44be2
Ammar Zahid <ammarzahid.work@gmail.com>
2025-03-21 16:07:54 +0500

    Integrated Extensions (Heading,Image,Table)

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,16 @@
 "use client";
 
 import { useEditor, EditorContent } from '@tiptap/react'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
 import StarterKit from '@tiptap/starter-kit'
+import { TooltipPortal } from '@radix-ui/react-tooltip';
 
 export const Editor = () => {
     const editor = useEditor({
@@ -11,8 +20,36 @@ export const Editor = () => {
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             },
         },
-        extensions: [StarterKit],
-        content: '<p>Hello World! 🌎️</p>',
+        extensions: [
+            StarterKit,
+            Image,
+            ImageResize,
+            Table,
+            TableCell,
+            TableHeader,
+            TableRow,
+            TaskItem.configure({
+                nested: true,
+            }),
+            TaskList,
+        ],
+        // content: '<p>Hello World! 🌎️</p>',
+        content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
       })
     
     return (
