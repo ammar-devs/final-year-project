305825682f5980e28a6aa4ad5c10b210b40dab0d
Ammar Zahid <ammarzahid.work@gmail.com>
2025-03-27 16:38:24 +0500

    Line Height Custom Built Extension

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,6 +21,7 @@ import { TooltipPortal } from '@radix-ui/react-tooltip';
 
 import { useEditorStore } from '@/store/use-editor-store';
 import { FontSizeExtension } from '@/extensions/font-size';
+import { LineHeightExtension } from '@/extensions/line-height';
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -57,6 +58,7 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      LineHeightExtension,
       FontSizeExtension,
       Link.configure({
         openOnClick: false,
