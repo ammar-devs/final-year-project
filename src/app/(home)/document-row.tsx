4e534b43d0e81ac24eb3b4717a50c14ae96c6b0b
Ammar Zahid <ammarzahid.work@gmail.com>
2025-04-07 10:41:30 +0500

    Update and Delete Documents

--- src/app/(home)/document-row.tsx
+++ src/app/(home)/document-row.tsx
@@ -1,17 +1,23 @@
 import { format } from "date-fns";
-import { Button } from "@/components/ui/button";
 import { TableCell, TableRow } from "@/components/ui/table";
+import { useRouter } from "next/navigation";
 import { Doc } from "../../../convex/_generated/dataModel"
 import { SiGoogledocs } from "react-icons/si";
-import { Building2Icon, CircleUserIcon, MoreVertical } from "lucide-react";
+import { Building2Icon, CircleUserIcon } from "lucide-react";
+import { DocumentMenu } from "./document-menu";
 
 interface DocumentRowProps {
     document: Doc<"documents">
 };
 
 export const DocumentRow = ({ document }: DocumentRowProps) => {
+    const router = useRouter();
+
+
     return (
-        <TableRow className="cursor-pointer"
+        <TableRow 
+            onClick={() => router.push(`/documents/${document._id}`)}
+            className="cursor-pointer"
         >
             <TableCell className="w-[50px]">
                 <SiGoogledocs className="size-6 fill-blue-500"/>                
@@ -31,9 +37,11 @@ export const DocumentRow = ({ document }: DocumentRowProps) => {
                 {format(new Date(document._creationTime), "MMM dd, yyy")}
             </TableCell>
             <TableCell className="flex justify-end">
-                <Button variant="ghost" size="icon" className="rounded-full">
-                    <MoreVertical className="size-4" />
-                </Button>
+                <DocumentMenu 
+                    documentId={document._id}
+                    title={document.title}
+                    onNewTab={() => window.open(`/documents/${document._id}`, "_blank")}
+                />
             </TableCell>    
         </TableRow>
     )
